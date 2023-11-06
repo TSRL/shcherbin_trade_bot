e86aaa1422dffa00145123bad8637901609c3020
Алексей <aleksei.shcherbin@espeo.eu>
2023-11-06 02:59:34 +0100

    added check for correct command and reduced number of workers to 1

--- trade_bot/start-prod.sh
+++ trade_bot/start-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 5 # waiting for database to be up, could be done properly with a management command, but that would increase complexity
 python manage.py migrate --noinput &&
-guinicorn --workers=8 --timeout=3000 --bind=0.0.0.0:8989 --name=trade_bot trade_bot.wsge:application
+gunicorn --workers=1 --timeout=3000 --bind=0.0.0.0:8989 --name=trade_bot trade_bot.wsgi:application
